67e7016453b935641c99f77a4157846cbb6c6137
Baptiste Szmoniewski <b.szmoniewski@orange.fr>
2024-02-07 15:40:21 +0100

    variable

---
+++ jementraine/variable.rb
@@ -0,0 +1,10 @@
+variable = "contenus de ma variable"
+puts variable
+
+puts "Quel est ton nom?"
+nom = "Baptiste"
+puts "mon nom est #{nom}"
+
+puts "Quel âge as-tu ?"
+age = gets.chomp
+puts "tu as " + age + " ans"
